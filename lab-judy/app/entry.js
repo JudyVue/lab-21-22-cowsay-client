070d12105a66c6a28952df3605b713e95b70046e
JudyVue <judyvue87@gmail.com>
2016-10-18 15:54:15 -0700

    finished lab 21-22 assignment

--- lab-judy/app/entry.js
+++ lab-judy/app/entry.js
@@ -8,25 +8,52 @@ const cowsay = require('cowsay-browser');
 const angular = require('angular');
 
 //angular module
+//the 2 args means this is a setter app that creates actual demoApp module
 const demoApp = angular.module('demoApp', []);
 
 //angular constructs
-demoApp.controller('CowsayController', [ '$log', '$scope', CowsayController]);
+demoApp.controller('CowsayController', [ '$log', CowsayController]);
 
-function CowsayController($log, $scope){
+function CowsayController($log){
   $log.debug('init CowsayController');
 
-  let cowsayCtrl = $scope.cowsayCtrl = {};
-  cowsayCtrl.title = 'Moooo';
+  this.title = 'Moooo';
+  this.history = [];
+  cowsay.list( (err, cowfiles) => {
+    this.cowfiles = cowfiles;
+    this.currentCow = this.cowfiles[0];
+    console.log(this.cowfiles);
+  });
 
-  cowsayCtrl.updateCow = function(input){
+  //renders cow to page with text from input field
+  this.updateCow = function(input){
     $log.debug('cowsayCtrl.updateCow()');
-
-    return '\n' + cowsay.say({text: input || 'type something dammit'});
+    return '\n' + cowsay.say({text: input || 'type something dammit', f: this.currentCow});
   };
 
-  cowsayCtrl.helloClick = function(input){
-    $log.debug('cowsayCtrl.helloClick()');
-    $log.log(input);
+  this.speak = function(input){
+    $log.debug('cowsayCtrl.updateCow()');
+    this.spoken = this.updateCow(input);
+    this.history.push(this.spoken);
   };
+  this.undo = function(){
+    $log.debug('this.undo()');
+    this.spoken = this.history.pop() || '';
+  };
+
+}
+
+demoApp.controller('NavController', ['$log', NavController]);
+function NavController($log){
+  $log.debug('NavController');
+  this.routes = [
+    {
+      name: 'Home',
+      url: '/home',
+    },
+    {
+      name: 'About',
+      url: '/about',
+    },
+  ];
 }
